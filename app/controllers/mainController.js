1c1142c064b1abc2c92cf7b0310610e26fb3d09b
noam <noam93k@gmail.com>
2017-03-09 10:02:43 +0200

    fix navigation buttons

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,4 +1,6 @@
-app.controller("mainController", function($scope, $mdDialog) {
+app.controller("mainController", function($scope, $mdDialog, $location) {
+    $scope.isMenuOpen = false;
+
     $scope.showAlert = function(ev) {
         $mdDialog.show(
             $mdDialog.alert()
@@ -9,5 +11,15 @@ app.controller("mainController", function($scope, $mdDialog) {
                 .ok('Got it!')
                 .targetEvent(ev)
         );
+    }
+
+    $scope.submitUserData = function() {
+        alert("Hello")
+        $location.path("/time");
+    }
+
+    $scope.goHome = function() {
+        alert("Hello")
+        $location.path("/");
     }
 })
